16340ecb5d9d03d856781d142526e1e05e19d211
heusyfelype <felype.heusy@gmail.com>
2022-04-30 19:04:05 -0300

    feat: construída as validações - nome deve ser uma string; se o usuário já existe imprime mensagem de erro

--- back-bate-papo-uol/index.js
+++ back-bate-papo-uol/index.js
@@ -1,8 +1,9 @@
 
 
 import express from 'express';
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import cors from "cors";
+import joi from 'joi';
 
 const app = express();
 app.use(cors());
@@ -17,9 +18,36 @@ promise.then(() => {
 })
 promise.catch( e => console.log("Não Foi possível conectar ao banco", e))
 
-app.get( "/", (req, res) =>{
+app.post( "/participants", async (req, res) =>{
+    const login = req.body;
+
+    const loginSchema = joi.object({
+        nome: joi.string().required(),
+    });
+
+    const validationLogin = loginSchema.validate(login, {abortEarly: false});
+
+    if(validationLogin.error){
+        console.log(validationLogin.error);
+        res.status(422).send(validationLogin.error.details.message)
+        return;
+    }
+
+    try{
+         const find = await dataBase.collection("users").findOne(login);
+         if(find === null){
+            let insert = await dataBase.collection("users").insertOne(login);
+            console.log("Insert: " + JSON.stringify(insert));
+        }
+         console.log("find: " + JSON.stringify(find));
+    } catch(e){
+        console.log("Deu Ruim: " + e)
+    }
+
+
+
     console.log("Está tudo indo")
-    res.send("Deu certo!")
+    res.send("Deu certo! ValidationLogin: " + JSON.stringify(validationLogin))
 })
 
 app.listen(5000);
