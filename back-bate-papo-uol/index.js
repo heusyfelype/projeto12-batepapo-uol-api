c2ff8c927d200cb1df8c5798d593b11a25c3f6c4
heusyfelype <felype.heusy@gmail.com>
2022-04-30 17:05:24 -0300

    feat: construida a estrutura básica do banco de dados

--- back-bate-papo-uol/index.js
+++ back-bate-papo-uol/index.js
@@ -4,10 +4,18 @@ import express from 'express';
 import { MongoClient } from "mongodb";
 import cors from "cors";
 
-//const mongoClient = new MongoClient("mongodb://localhost:27017");
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+let dataBase;
+const mongoClient = new MongoClient("mongodb://localhost:27017");
+const promise = mongoClient.connect();
+promise.then(() => {
+    dataBase = mongoClient.db("bate-papo-uol");
+    console.log("Conectado ao banco de dados!");
+})
+promise.catch( e => console.log("Não Foi possível conectar ao banco", e))
 
 app.get( "/", (req, res) =>{
     console.log("Está tudo indo")
