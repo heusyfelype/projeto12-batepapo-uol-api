53ffe76f4a19e07407d5459845f4703d3238ae3a
heusyfelype <felype.heusy@gmail.com>
2022-04-30 16:39:08 -0300

    feat: construida a estrutura básica de servidor

--- back-bate-papo-uol/index.js
+++ back-bate-papo-uol/index.js
@@ -1,1 +1,17 @@
-//Teste
+
+
+import express from 'express';
+import { MongoClient } from "mongodb";
+import cors from "cors";
+
+//const mongoClient = new MongoClient("mongodb://localhost:27017");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get( "/", (req, res) =>{
+    console.log("Está tudo indo")
+    res.send("Deu certo!")
+})
+
+app.listen(5000);
